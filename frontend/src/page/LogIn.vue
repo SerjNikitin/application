e43d0a2d1414d417d337c52712040fa560df7167
PokorskyP <PokorskyP@fizpribor.ru>
2022-02-16 20:34:45 +0300

    change rout + create ctarted components

---
+++ frontend/src/page/LogIn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-modal"></div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.login-modal {
+  position: absolute;
+  top: 10%;
+  left: 20%;
+  width: 60%;
+  height: 70%;
+  background-color: bisque;
+  border-radius: 10px;
+}
+</style>
