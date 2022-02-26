e7f402bd6f72ad26d9f27cd7bf3e1ac853832061
NikitinS <NikitinS@fizpribor.ru>
2022-02-26 15:47:39 +0300

    update properties and created test controller

--- backend/client-eureka-service/src/main/java/com/example/clienteurekaservice/ClientEurekaServiceApplication.java
+++ backend/client-eureka-service/src/main/java/com/example/clienteurekaservice/ClientEurekaServiceApplication.java
@@ -2,11 +2,12 @@ package com.example.clienteurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @RefreshScope
-@EnableDiscoveryClient
+//@LoadBalancerClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ClientEurekaServiceApplication {
 
---
+++ backend/client-eureka-service/src/main/java/com/example/clienteurekaservice/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.clienteurekaservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+	@GetMapping("/test")
+	public String test(){
+		return "test";
+	}
+
+	@GetMapping("/check")
+	public String check(){
+		return "check";
+	}
+}
